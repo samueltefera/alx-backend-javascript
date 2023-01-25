e82bd4e1a2e6742f2dbd06f60dc549390e3ae5af
samueltefera <samueltefer23@gmail.com>
2023-01-25 23:04:16 +0300

    task done

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,7 +1,7 @@
 export default function guardrail(mathFunction) {
   const queue = [];
   try {
-    queue.push(mathFunction());
+    queue.push(mathFunction(), 'Guardrail was processed');
   } catch (err) {
     queue.push(`Error: ${err.message}`, 'Guardrail was processed');
   }
