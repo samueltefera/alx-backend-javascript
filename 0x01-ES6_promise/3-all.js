4c96f3cade166cb28e69544acee5bb98f45cf23b
samueltefera <samueltefer23@gmail.com>
2023-01-25 21:04:05 +0300

    task done

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { createUser, uploadPhoto } from "./utils";
+
+export default function handleProfileSignup() {
+  const api1 = uploadPhoto();
+  const api2 = createUser();
+
+  Promise.all([api1, api2]).then((data) => {
+    const { body } = data[0];
+    console.log(`${body} ${data[1].firstName} ${data[1].lastName}`)
+  });
+}
