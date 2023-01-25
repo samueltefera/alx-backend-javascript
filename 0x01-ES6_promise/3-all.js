4bd42696c8d4ae0718291b424079f2221534cb7b
samueltefera <samueltefer23@gmail.com>
2023-01-25 21:12:09 +0300

    task done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,18 @@
-import { createUser, uploadPhoto } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const api1 = uploadPhoto();
-  const api2 = createUser();
+  let firstName;
+  let lastName;
+  let body;
 
-  return Promise.all([api1, api2]).then((data) => {
-    const { body } = data[0];
-    console.log(`${body} ${data[1].firstName} ${data[1].lastName}`)
+  const p = uploadPhoto();
+  const u = createUser();
+
+  return Promise.all([p, u]).then((data) => {
+    body = data[0].body;
+    firstName = data[1].firstName;
+    lastName = data[1].lastName;
+    console.log(`${body} ${firstName} ${lastName}`);
   }).catch(() => {
     console.log('Signup system offline');
   });
