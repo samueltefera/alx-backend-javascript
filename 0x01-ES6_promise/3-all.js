108a3619bf751f77a73248ab5a6a7ffc36154878
samueltefera <samueltefer23@gmail.com>
2023-01-25 21:07:32 +0300

    task done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -7,5 +7,7 @@ export default function handleProfileSignup() {
   Promise.all([api1, api2]).then((data) => {
     const { body } = data[0];
     console.log(`${body} ${data[1].firstName} ${data[1].lastName}`)
+  }).catch(() => {
+    console.log('ddd');
   });
 }
