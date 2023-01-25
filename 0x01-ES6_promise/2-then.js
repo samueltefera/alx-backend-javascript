4b264417cdaaf6053d51a99c3279d63e812893f1
samueltefera <samueltefer23@gmail.com>
2023-01-25 20:43:36 +0300

    task done

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => {
-    console.log('Got a response from the API');
-  });
+  const obj = { status: 200, body: 'success' };
+  return promise
+    .then(() => obj)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
