8da2bc97737ccecb9b2b116966d0b71fff3e3467
samueltefera <samueltefer23@gmail.com>
2023-01-25 22:36:19 +0300

    task done

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,24 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUp = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
+  return Promise.allSettled([signUp, upload]).then((vals) => {
+    const resArr = [];
+    vals.forEach((val) => {
+      if (val.status === 'fulfilled') {
+        resArr.push({ status: val.status, value: val.value });
+      } else {
+        resArr.push(
+          {
+            status: val.status,
+            value: `Error: ${val.reason.message}`,
+
+          },
+        );
+      }
+    });
+    return resArr;
+  }, (ers) => ers);
+}
